98b54901ca67f373f3c9937de873f3daf826c428
Kristin Merbach <kristin@kristinmerbach.com>
2022-07-20 07:50:51 -0700

    entity definition

--- app/entities/api/v1/extract_entity.rb
+++ app/entities/api/v1/extract_entity.rb
@@ -1,19 +1,18 @@
 # frozen_string_literal: true
 
-module Entities
-  module Api
-    module V1
-      # entity for extract for use in validations
-      class ExtractEntity < Dry::Struct
-        attribute :provider_identifier, Types::String
-        attribute :npi, Types::String.optional.meta(omittable: true)
-        attribute :coverage_start, Types::Date.optional.meta(omittable: true)
-        attribute :coverage_end, Types::Date.optional.meta(omittable: true)
-        attribute :extracted_on, Types::Date.optional.meta(omittable: true)
-        attribute :file_name, Types::String.optional.meta(omittable: true)
-        attribute :file_type, Types::String.optional.meta(omittable: true)
-        attribute :payload, Types::Hash.optional.meta(omittable: true)
-      end
+module Api
+  module V1
+    # entity for extract for use in validations
+    class ExtractEntity < Dry::Struct
+      attribute :provider_identifier, Types::String
+      attribute :npi, Types::String.optional.meta(omittable: true)
+      attribute :coverage_start, Types::Date.optional.meta(omittable: true)
+      attribute :coverage_end, Types::Date.optional.meta(omittable: true)
+      attribute :extracted_on, Types::Date.optional.meta(omittable: true)
+      attribute :file_name, Types::String.optional.meta(omittable: true)
+      attribute :file_type, Types::String.optional.meta(omittable: true)
+      attribute :payload, Types::Hash.optional.meta(omittable: true)
     end
   end
 end
+
