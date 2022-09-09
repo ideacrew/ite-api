1f8bd305db4b201f04c55c0d3efcdc38e33e76b5
Kristin Merbach <kristin@kristinmerbach.com>
2022-09-09 08:38:42 -0700

    401 if authentication issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
+# application controller
 class ApplicationController < ActionController::API
   include RailsJwtAuth::AuthenticableHelper
   include Pundit::Authorization
+
+  rescue_from RailsJwtAuth::NotAuthorized do |_exception|
+    render json: { status_text: 'Could not authenticate', status: 401, content_type: 'application/json' }
+  end
 end
