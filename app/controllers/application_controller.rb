812df65c8fb1b67d4090ac99598377ed22121a56
Kristin Merbach <kristin@kristinmerbach.com>
2022-09-13 07:09:06 -0700

    move status code to header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::API
   include Pundit::Authorization
 
   rescue_from RailsJwtAuth::NotAuthorized do |_exception|
-    render json: { status_text: 'Could not authenticate', status: 401, content_type: 'application/json' }
+    render json: { status_text: 'Could not authenticate', content_type: 'application/json' }, status: 401
   end
 end
