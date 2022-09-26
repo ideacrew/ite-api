6606e1d4250e53f7d5a6cae604aceb7c9ff0ec4c
Haridhar Yamjala <hariym471@gmail.com>
2022-09-26 13:35:12 -0400

    Specs fixes.

--- spec/validators/api/v1/episode_contract_spec.rb
+++ spec/validators/api/v1/episode_contract_spec.rb
@@ -86,7 +86,7 @@ RSpec.describe ::Validators::Api::V1::EpisodeContract, dbclean: :around_each do
       sud_diagnostic_codes: ['12'],
       mh_diagnostic_codes: ['19'],
       sud_dx1: 'F14.8393',
-      mh_dx1: 'F14.8393',
+      mh_dx1: 'F24.8393',
       sud_dx2: 'F14.8393'
     }
   end
